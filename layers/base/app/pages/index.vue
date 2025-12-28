9c3a92c6d8e2a2750394b16cd267bcbaf9430db5
lotva <dlotva@gmail.com>
2025-12-28 17:31:03 +0300

    feat: initialize project

---
+++ layers/base/app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<SampleComponent />
+
+		<NuxtLink to="/sample-page">Sample Page</NuxtLink>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import SampleComponent from '../common/ui/SampleComponent.vue'
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+export default defineNuxtConfig({
+	compatibilityDate: '2025-07-15',
+
+	css: ['~~/layers/base/app/core/assets/styles/index.css'],
+
+	devtools: { enabled: true },
+
+	dir: {
+		assets: './layers/base/app/core/assets',
+	},
+
+	eslint: {
+		config: {
+			standalone: false,
+		},
+	},
+	experimental: {
+		typedPages: true,
+	},
+	extends: ['./layers/base'],
+
+	imports: {
+		scan: false,
+	},
+
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/test-utils',
+		'@nuxt/image',
+		'@nuxt/devtools',
+	],
+
+	typescript: {
+		tsConfig: {
+			include: ['../commitlint.config.ts'],
+		},
+	},
+})
